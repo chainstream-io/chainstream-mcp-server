cdb71dee30274e2c47cee29bbc7a8be6d7216fb6
harry5556 <haharry555@proton.me>
2025-10-18 17:01:40 +0800

    format

--- playground/resources/blockchain/blockchain.resource.ts
+++ playground/resources/blockchain/blockchain.resource.ts
@@ -15,45 +15,6 @@ type SupportedChain =
   | 'zksync'
   | 'sui';
 
-// Define supported sort fields
-type SortByField =
-  | 'marketCapInUsd'
-  | 'liquidityInUsd'
-  | 'priceInUsd'
-  | 'holderCount'
-  | 'h24VolumeInUsd'
-  | 'h24Transactions'
-  | 'tokenCreatedAt';
-
-type SortByFields =
-  | 'marketData.priceInUsd'
-  | 'stats.priceChangeRatioInUsd1m'
-  | 'stats.priceChangeRatioInUsd5m'
-  | 'stats.priceChangeRatioInUsd1h'
-  | 'stats.priceChangeRatioInUsd4h'
-  | 'stats.priceChangeRatioInUsd24h'
-  | 'marketData.marketCapInUsd'
-  | 'marketData.tvlInUsd'
-  | 'marketData.top10HoldingsRatio'
-  | 'marketData.top100HoldingsRatio'
-  | 'marketData.holders'
-  | 'stats.trades1m'
-  | 'stats.trades5m'
-  | 'stats.trades1h'
-  | 'stats.trades4h'
-  | 'stats.trades24h'
-  | 'stats.traders1m'
-  | 'stats.traders5m'
-  | 'stats.traders1h'
-  | 'stats.traders4h'
-  | 'stats.traders24h'
-  | 'stats.volumesInUsd1m'
-  | 'stats.volumesInUsd5m'
-  | 'stats.volumesInUsd1h'
-  | 'stats.volumesInUsd4h'
-  | 'stats.volumesInUsd24h'
-  | 'tokenCreatedAt';
-
 @Injectable({ scope: Scope.REQUEST })
 export class BlockchainResource {
   @ResourceTemplate({
